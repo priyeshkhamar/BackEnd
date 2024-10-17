d1c809d8b1495b6bdfc92561e7afc4bacfeac7ad
priyeshkhamar <priyeshkhamar76@gmail.com>
2024-10-17 11:30:29 +0530

    Integration

--- Assignments/Assignment - 4 [Integration with Frontend]/AuthApi/Backend/index.js
+++ Assignments/Assignment - 4 [Integration with Frontend]/AuthApi/Backend/index.js
@@ -10,6 +10,10 @@ app.use(express.json());
 
 app.use(userRouter);
 
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+
 mongoose.connect('mongodb://localhost:27017/session_management');
 
 app.listen(port, () => {
